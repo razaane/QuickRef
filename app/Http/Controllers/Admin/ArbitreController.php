d699d6500c5a22dc8422cbe6db52184a437962ee
razaane <wakhidirazane@gmail.com>
2026-03-31 16:12:13 +0100

    l'ajout des fonctions create and store in the arbitres controller

--- app/Http/Controllers/Admin/ArbitreController.php
+++ app/Http/Controllers/Admin/ArbitreController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+
 
 class ArbitreController extends Controller
 {
@@ -21,7 +23,7 @@ class ArbitreController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.arbitres.create');
     }
 
     /**
@@ -29,7 +31,29 @@ class ArbitreController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name'=>'required|string|max:50',
+            'email'=>'required|string|email|max:255|unique:users',
+            'password'=>'required|string|min:8',
+            'telephone'=>'required|string|max:20',
+            'grade'=>'required|in:regional,national,international',
+        ]);
+        DB::transation(function() use ($request){
+            $user=User::create([
+                'name'=>$request->name,
+                'email'=>$request->email,
+                'password'=>Hash::make($request->password),
+                'role'=>'arbitre',
+            ]);
+
+            Arbitre::create([
+                'user_id'=>$user->id,
+                'telephone'=>$request->telephone,
+                'grade'=>$request->grade,
+                'adresse'=>$request->adresse,
+            ]);
+        });
+        return redirect()->route('admin.arbitres.index')->with('success','Arbitre ajouter avec success');
     }
 
     /**
@@ -37,7 +61,7 @@ class ArbitreController extends Controller
      */
     public function show(string $id)
     {
-        //
+        
     }
 
     /**
